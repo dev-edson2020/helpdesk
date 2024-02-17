d8ef2c7fabf23eb5aabb11489ebf3eb434f73a12
Edson Meira <edsonanga@hotmail.com>
2024-02-17 17:24:16 -0300

    Repositories criados

--- src/main/java/com/edson/helpdesk/domain/Cliente.java
+++ src/main/java/com/edson/helpdesk/domain/Cliente.java
@@ -8,7 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.edson.helpdesk.domain.dtos.ClienteDTO;
+//import com.edson.helpdesk.domain.dtos.ClienteDTO;
 import com.edson.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -29,16 +29,16 @@ public class Cliente extends Pessoa {
 		addPerfil(Perfil.CLIENTE);
 	}
 
-	public Cliente(ClienteDTO obj) {
-		super();
-		this.id = obj.getId();
-		this.nome = obj.getNome();
-		this.cpf = obj.getCpf();
-		this.email = obj.getEmail();
-		this.senha = obj.getSenha();
-		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
-		this.dataCriacao = obj.getDataCriacao();
-	}
+//	public Cliente(ClienteDTO obj) {
+//		super();
+//		this.id = obj.getId();
+//		this.nome = obj.getNome();
+//		this.cpf = obj.getCpf();
+//		this.email = obj.getEmail();
+//		this.senha = obj.getSenha();
+//		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+//		this.dataCriacao = obj.getDataCriacao();
+//	}
 
 	public List<Chamado> getChamados() {
 		return chamados;
